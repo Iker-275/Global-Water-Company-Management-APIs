b011503db2c2965a1c6e9ca903527b740b6388c5
Iker 275 <jeananderson275@gmail.com>
2026-03-10 01:21:40 +1000

    notification-user resolution per role now working

--- services/notificationService.js
+++ services/notificationService.js
@@ -1,20 +1,55 @@
 
-const Notification = require("../models/notificationModel");
+// const Notification = require("../models/notificationModel");
 
- const createNotification = async ({
+//  const createNotification = async ({
+//   type,
+//   message,
+//   targetRoles = [],
+//   targetUsers = [],
+//   relatedEntity = null
+// }) => {
+//   return await Notification.create({
+//     type,
+//     message,
+//     targetRoles,
+//     targetUsers,
+//     relatedEntity
+//   });
+// };
+
+// module.exports= {createNotification};
+
+
+const Notification = require("../models/notificationModel");
+const User = require("../models/userModel");
+
+const createNotification = async ({
   type,
   message,
   targetRoles = [],
   targetUsers = [],
   relatedEntity = null
 }) => {
+  let resolvedUsers = targetUsers;
+
+  if (targetRoles?.length) {
+    const users = await User.find({
+      role: { $in: targetRoles },
+      deletedAt: null
+    }).select("_id");
+
+    resolvedUsers = [
+      ...new Set([...resolvedUsers, ...users.map(u => u._id.toString())])
+    ];
+  }
+
   return await Notification.create({
     type,
     message,
     targetRoles,
-    targetUsers,
+    targetUsers: resolvedUsers,
     relatedEntity
   });
 };
 
-module.exports= {createNotification};
+module.exports = { createNotification };
